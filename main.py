1f9cb9a5b8217eeebb5c3647f1d05c20653f9f37
TrakeLean <91937397+TrakeLean@users.noreply.github.com>
2022-05-31 18:25:19 +0200

    last push was not a fix :D

--- main.py
+++ main.py
@@ -5,16 +5,9 @@ import lightbulb
 import aiohttp
 from webserver import keep_alive
 from constants import CONSTANTS
-keep_alive()
-
-
 
 # Create the main bot instance with all intents.
 bot = CONSTANTS.bot
-
-
-
-
     
 @bot.listen()
 async def on_starting(event: hikari.StartingEvent) -> None:
@@ -34,18 +27,13 @@ async def on_message_create(event: hikari.GuildMessageCreateEvent) -> None:
             f"Latency: {bot.heartbeat_latency*1000:.2f}ms"
         )
 
-
-   
 # Load all extensions.
 bot.load_extensions_from("./bot-commands")
-
-
-
-
 
 if __name__ == "main":
     if os.name != "nt":
         import uvloop
 
         uvloop.install()
+keep_alive()
 bot.run()
